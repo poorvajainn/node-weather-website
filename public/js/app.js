6dbdfc5bd10746089e3239a0d91759731b2d83b5
unknown <poorva.jain1998@gmail.com>
2020-05-31 04:05:13 +0530

    Set for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener('submit', function(e){
     e.preventDefault()
 
     const location =search.value
-    fetch('http://localhost:3000/weather?address='+location).then(function(response){
+    fetch('/weather?address='+location).then(function(response){
         response.json().then(function(data){
             if(data.error){
                 messageOne.textContent=data.error
